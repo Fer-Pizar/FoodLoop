c98f8767f36fe3a4975ced343a3b8b2ddb32e6f8
Fer-Pizar <mafer.pizarroso@gmail.com>
2025-10-01 21:15:40 -0400

    Front and Back connected with CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['http://localhost:19006', 'http://localhost:8081'], // Expo Web & Metro
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
