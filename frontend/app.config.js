1c83734e7eab950134ae7896daa7c6cd539a4249
Fer-Pizar <mafer.pizarroso@gmail.com>
2025-11-23 21:39:06 -0400

    details

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,8 +1,11 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
   extra: {
+    // keep anything that was defined in app.json
+    ...config.extra,
+    // and then add/override your custom env variables
     BACKEND_URL: process.env.EXPO_PUBLIC_API_URL,
   },
 });
